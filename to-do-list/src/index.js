8113e0925cedb6007697398285c6894596a74618
Andrii <demonictoast@gmail.com>
2017-04-23 17:26:17 +0300

    Created ToDo component. just some ui for now

---
+++ to-do-list/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import ToDoList from './ToDO';
+
+ReactDOM.render(
+  <ToDoList />,
+  document.getElementById('root')
+);
